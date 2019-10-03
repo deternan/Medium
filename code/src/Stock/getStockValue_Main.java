eec9928e957ba2460f250a5bfbe51bc4f82d03d8
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-10-03 18:40:01 +0800

    revised format

--- code/src/Stock/getStockValue_Main.java
+++ code/src/Stock/getStockValue_Main.java
@@ -3,7 +3,7 @@ package Stock;
 /*
  * Parser Stock value by Date
  * version: October 02, 2019 09:30 PM
- * Last revision: October 03, 2019 06:56 AM
+ * Last revision: October 03, 2019 06:36 PM
  * 
  * Author : Chao-Hsuan Ke
  * E-mail : phelpske.dev at gmail dot com
@@ -13,6 +13,7 @@ package Stock;
 
 public class getStockValue_Main 
 {
+	String startDate = "20180101";			// start date
 	private static int sleepTime = 5200;	// 5.2 secs
 	private String sourceFolder = "";
 	private String StockValue = "";
@@ -20,11 +21,7 @@ public class getStockValue_Main
 	
 	public getStockValue_Main() throws Exception
 	{
-		
-		String startDate = "20180806";		// start date
-		
 		GetValueandProcessing_StockValue stockvalue = new GetValueandProcessing_StockValue(startDate, sourceFolder, StockValue, sleepTime);
-		
 	}
 	
 	public static void main(String args[])
