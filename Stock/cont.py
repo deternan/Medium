dbae28b956af86d12eba6382ba87b8f6b2662adf
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-12-02 18:58:17 +0800

    get end word count

--- Stock/cont.py
+++ Stock/cont.py
@@ -2,8 +2,8 @@
 # coding=utf8
 
 '''
-Created on December 02, 2020   07:30 PM
-Last revision: December 02, 2020   09:30 PM
+Created on December 02, 2020   06:30 PM
+Last revision: December 02, 2020   06:52 PM
 author: Chao-Hsuan Ke
 
 '''
@@ -18,20 +18,33 @@ filePath = "D:\\Phelps\\GitHub\\Medium\\Stock\\";        # file path
 fileName = "TWSETPEX.txt";        # file name
 
 f = open(filePath + fileName, 'r', encoding="utf-8")
-#print(f.read())                 # print all at once time
-#print(len(f.readlines()))      # all line number
+
 
 # read each lines
 count = 0
 idList = []
 nameList = []
+removeWord = ''
 for line in f:
     #print(line[0])
     sep = line.split('\t')
 #    print(count, sep[0], sep[1])
     idList.append(sep[0])
-    nameList.append(sep[1])
-    count+=1
+
+    '''
+    remove -KY
+    '''
+    if 'KY' in sep[1]:
+        removeWord = sep[1].replace('-KY', '')
+    else:
+        removeWord = sep[1]
+
+    removeWord.replace("\n", '')
+    removeWord.replace("\r", '')
+    nameList.append(removeWord)
+    #print(sep[0], len(removeWord.strip()), removeWord.strip()[-1])
+    count += 1
+
 
 #print(len(idList))
 #print(len(nameList))
@@ -43,18 +56,30 @@ get first work (Chinese work)
 '''
 
 count = 0
-firstWork = []
+firstWord = []
 for index in idList:
-    print(index, nameList[count])
-    firstWork.append(nameList[count][0])
+    #print(index, nameList[count])
+    firstWord.append(nameList[count][0])
     count+=1
-
-#print(firstWork)
 
 '''
 repeat word count
 '''
 
-print(Counter(firstWork))
+#print(Counter(firstWord))
+
+'''
+get last work (Chinese work) 
+'''
+
+count = 0
+lastWord = []
+for index in idList:
+    lastWord.append(nameList[count].strip()[-1])
+    count+=1
 
 
+'''
+repeat word count
+'''
+print(Counter(lastWord))
